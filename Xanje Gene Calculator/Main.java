5c0e065f68e7c21fa0dc4e21b56fda42b52814b3
Evan Poon <109251468+StewartChicken@users.noreply.github.com>
2022-12-06 21:45:02 -0700

    Create Main.java

---
+++ Xanje Gene Calculator/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Created by Evan Poon 
+ * Calculates genetic sequences for the offspring of 'bred'
+ * animals for Xanje
+ * First started 2022-03-12
+ */
+
+
+import java.awt.Graphics;  // Imports
+import java.awt.Graphics2D;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.swing.JFrame;
+
+// Main class
+public class Main{
+
+	// Main method
+	// Throws the IO and ClassNotFound Exceptions should there be an error with serialization (save function)
+	public static void main(String[] args) throws ClassNotFoundException, IOException { 
+
+		// Creates a "window" object that starts the program. 
+		MainWindow window = new MainWindow();
+	}
+	
+
+}
